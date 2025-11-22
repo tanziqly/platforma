d026ec8afd064c41eb531684f5f6d87cb892a27e
Bozhenashalginskaia <laskina15@mail.ru>
2025-11-22 16:07:17 +0300

    fix pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,11 +14,9 @@ import { SignIn } from "./SignIn";
 import { SignUp } from "./SignUp";
 import { Chat } from "./Chat";
 import { Team } from "./Team";
-<<<<<<< HEAD
 import { Study } from "./Study";
-=======
 import { Admin } from "./Admin";
->>>>>>> platforma/main
+
 
 // Header and Footer
 import { Layout } from "@widgets/Layout";
@@ -50,6 +48,7 @@ const AppRoutes: FC = () => {
         <Route path={ROUTE_CONSTANTS.TEAM} element={<Team />} />
         <Route path={ROUTE_CONSTANTS.STUDY} element={<Study />} />
         <Route path={ROUTE_CONSTANTS.SIGN_UP} element={<SignUp />} />
+        <Route path={ROUTE_CONSTANTS.ADMIN} element={<Admin />} />
       </Routes>
     </Layout>
   );
@@ -58,24 +57,7 @@ const AppRoutes: FC = () => {
 export const Router: FC = () => {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
       <AppRoutes />
-=======
-      <Layout>
-        <Routes>
-          <Route path={ROUTE_CONSTANTS.HOME} element={<Home />} />
-          <Route path={ROUTE_CONSTANTS.ABOUT} element={<About />} />
-          <Route path={ROUTE_CONSTANTS.PROFILE} element={<Profile />} />
-          <Route path={ROUTE_CONSTANTS.PRODUCTS} element={<Products />} />
-          <Route path={ROUTE_CONSTANTS.SERVICES} element={<Services />} />
-          <Route path={ROUTE_CONSTANTS.SIGN_IN} element={<SignIn />} />
-          <Route path={ROUTE_CONSTANTS.SIGN_UP} element={<SignUp />} />
-          <Route path={ROUTE_CONSTANTS.CHAT} element={<Chat />} />
-           <Route path={ROUTE_CONSTANTS.TEAM} element={<Team />} />
-          <Route path={ROUTE_CONSTANTS.ADMIN} element={<Admin />} />
-        </Routes>
-      </Layout>
->>>>>>> platforma/main
     </BrowserRouter>
   );
 };
